fdfe3151412fe567a172edbe2f5d5e454666d9a6
Kojima Lucas <kojimalucas43@gmail.com>
2026-06-02 09:40:40 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import s from './App.module.css'
 import { api } from './constants/api'
 import { useState, useEffect } from 'react'
-import logo from '/rm.png'
+import logo from '/logo.png'
 
 function App() {
   const [data,setData] = useState([])
+  const [name,setName] = useState("")
+  const [page,setPage] = useState()
 
   useEffect(() => {
-    api.get(`/character`).then((response) => {
+    api.get(`/character/?page=${page}&name=${name}`).then((response) => {
       setData(response.data.results)
     }).catch((error) => {
       console.error("Deu ruim pô", error)
     })
-  }, [])
+  }, [page,name])
   
   return (
     <>
      <img className = {s.logo} src={logo} alt='Logo Rick and Morty'/>
      <div>
         <label htmlFor=''>Search name</label>
-        <input type='text' placeholder='Type the name you want'/>
+        <input type='text' placeholder='Type the name you want' value={name} onChange={(e) => setName(e.target.value)}/>
+     </div>
+     <div>
+        <label htmlFor=''>Choose page</label>
+        <input type='number' placeholder='Type the page 1/42' value={page} onChange={(e) => setPage(e.target.value)}/>
      </div>
      <main>
         {data.map((item, index) => {
